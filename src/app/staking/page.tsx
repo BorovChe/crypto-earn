8cb5e5dbedb19620fbfadaf82dfc603dec72b655
BorovChe <roman2000borov@gmail.com>
2025-08-06 21:07:16 +0300

    fix

--- src/app/staking/page.tsx
+++ src/app/staking/page.tsx
@@ -8,19 +8,22 @@ export const dynamic = "force-dynamic";
 const StakingPage = async () => {
   const stakingList: IStakingData[] | null = await getStakingList();
 
+  if (!stakingList) {
+    return <div>Ошибка загрузки. Попробуйте позже</div>;
+  }
+
   return (
     <div className="py-4">
       <h1 className="mb-4 text-xl text-center font-bold">Staking Page</h1>
       <ul>
-        {stakingList &&
-          stakingList.map((data, index) => (
-            <li key={index} className="flex justify-center gap-10">
-              <p>{data.coin}</p>
-              <Link href={data.exchange.link}>{data.exchange.title}</Link>
-              <p>{data.apy}</p>
-              <p>{data.type}</p>
-            </li>
-          ))}
+        {stakingList.map((data, index) => (
+          <li key={index} className="flex justify-center gap-10">
+            <p>{data.coin}</p>
+            <Link href={data.exchange.link}>{data.exchange.title}</Link>
+            <p>{data.apy}</p>
+            <p>{data.type}</p>
+          </li>
+        ))}
       </ul>
     </div>
   );
